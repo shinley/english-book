946b23e3e6d74f74b9e3e78e31ce102571676607
shinley <cxl17258@qq.com>
2019-08-12 22:23:39 +0800

    添加部分内容

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: '学英语',
+    description: '用来学英语的静态站点',
+    themeConfig: {
+        sidebarDepth: 2,
+        sidebar: [
+            {
+                title: "生活英语",
+                collapsable: false,
+                children: [
+                    '/life-english/chapter1/1.生活琐事'
+                ]
+            }
+        ]
+    }
+}
