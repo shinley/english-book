66b088adf4d080dfd3a1e0d2ce9ee4da45f8cc7c
shinley <cxl17258@qq.com>
2019-08-25 17:08:34 +0800

    增加第二章内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,8 @@ module.exports = {
                 title: "生活英语",
                 collapsable: false,
                 children: [
-                    '/life-english/chapter1/1.生活琐事'
+                    '/life-english/chapter1/1.生活琐事',
+                    '/life-english/chapter1/2.个人习惯'
                 ]
             }
         ]
